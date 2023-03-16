8fe46322e45874525bf60a1e5eff5ae0f011b5cb
Nick Faulkner <126799172+ItsMeLebo@users.noreply.github.com>
2023-03-16 15:19:11 +0000

    uploaded website in a close to finished state, ready for the addition of content

---
+++ scripts/quizflip.js
@@ -0,0 +1,19 @@
+/*
+const card = document.getElementById("card")
+
+card.addEventListener("click",flipCard);
+
+function flipCard() {
+    card.classList.toggle("flipCard")
+}
+*/
+const cards = document.querySelectorAll(".card");
+
+for (let i = 0; i < cards.length; i++) {
+    const card = cards[i];
+    card.addEventListener("click", flipCard);
+}
+
+function flipCard() {
+    this.classList.toggle("flipCard");
+}
